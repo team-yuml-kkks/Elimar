e4b8f2fa143a6244aa2fcc13311f757e74f1493a
Kamil Kucharski <kamil.kucharski@makimo.pl>
2023-01-31 21:35:04 +0100

    Setup docker compose integration for development

--- elimar/settings.py
+++ elimar/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +76,9 @@ WSGI_APPLICATION = "elimar.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
+DATABASES["default"] = dj_database_url.parse(os.environ["ELIMAR_DATABASE_URL"], conn_max_age=600)
+DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql"
 
 
 # Password validation
