4fff7570bb3281c78fa6e7d16b06a8fd8cd98abe
Kamil Kucharski <kamil.kucharski@makimo.pl>
2023-02-05 15:57:44 +0100

    Install users app & generate custom user model

--- elimar/settings.py
+++ elimar/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "elimar.modules.users",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
