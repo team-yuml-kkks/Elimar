d1f1987d83c0c8fa4fff59a9159185504a94ff5a
Kamil Kucharski <kamil.kucharski@makimo.pl>
2023-02-19 09:03:33 +0100

    Format files

--- elimar/modules/users/models.py
+++ elimar/modules/users/models.py
@@ -1,3 +1,4 @@
+# ruff: noqa: ANN003, ANN101, ANN201
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -6,24 +7,13 @@ from django.db import models
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError("Users require an email field")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email: str, password: str, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
 
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email: str, password: str, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -35,12 +25,23 @@ class UserManager(BaseUserManager):
 
         return self._create_user(email, password, **extra_fields)
 
+    def _create_user(self, email: str, password: str, **extra_fields):
+        if not email:
+            raise ValueError("Users require an email field")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  # type: ignore
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractUser):
-    username = None
+    username = None  # type: ignore
     email = models.EmailField("E-mail address", unique=True)
 
-    objects = UserManager()
+    objects = UserManager()  # type: ignore
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
