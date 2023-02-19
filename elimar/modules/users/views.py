ab74913acd4dda672b137b271735f08e325f6952
Kamil Kucharski <kamil.kucharski@makimo.pl>
2023-02-19 09:37:38 +0100

    Block first sign up view when at least one user exist

--- elimar/modules/users/views.py
+++ elimar/modules/users/views.py
@@ -1,4 +1,6 @@
-from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import FirstUserSignUpForm
@@ -8,3 +10,9 @@ class FirstUserSignUpView(CreateView):
     form_class = FirstUserSignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/first_user_sign_up.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if get_user_model().objects.first():
+            return redirect(reverse("home"))
+        
+        return super().dispatch(request, *args, **kwargs)
