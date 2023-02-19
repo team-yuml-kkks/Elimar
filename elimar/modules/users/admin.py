0d57412c58cf6c1300e8f31d1368833039ccf8ac
Kamil Kucharski <kamil.kucharski@makimo.pl>
2023-02-19 08:50:01 +0100

    Make email field as username field & remove username

--- elimar/modules/users/admin.py
+++ elimar/modules/users/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm
 
 from .models import User
 
 
-class CustomUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = User
-
-
-class CustomUserAdmin(UserAdmin):
-    form = CustomUserChangeForm
-
-
-admin.site.register(User, CustomUserAdmin)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
